af86ef50232e1d2932e41eac46fe0c17be33d45b
akadrunya <andriy.koltsov@icloud.com>
2016-07-12 16:02:59 +0300

    2

--- src/main/java/ua/drunya/Leetcode2.java
+++ src/main/java/ua/drunya/Leetcode2.java
@@ -1,4 +1,58 @@
 package ua.drunya;
 
 public class Leetcode2 {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode head = null;
+        ListNode current = null;
+        boolean carried = false;
+        while (l1 != null || l2 != null) {
+
+            Integer v1 = l1 != null ? l1.val : 0;
+            Integer v2 = l2 != null ? l2.val : 0;
+
+            Integer sum = carried ? 1 + v1 + v2 : v1 + v2;
+
+            if (carried) {
+                current.val = sum % 10;
+            } else {
+                ListNode newNode = new ListNode(sum % 10);
+                if (current != null) {
+                    current.next = newNode;
+                    current = newNode;
+                } else {
+                    current = newNode;
+                    head = current;
+                }
+            }
+
+            carried = sum > 9;
+
+            if (carried) {
+                current.next = new ListNode(1);
+                current = current.next;
+            }
+
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null ? l2.next : null;
+
+        }
+
+        return head;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return val + (next != null ? " -> " + next : "");
+        }
+    }
 }
--- src/test/java/ua/drunya/Leetcode2Test.java
+++ src/test/java/ua/drunya/Leetcode2Test.java
@@ -1,7 +1,27 @@
 package ua.drunya;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import ua.drunya.Leetcode2.ListNode;
+
+import static org.junit.Assert.assertEquals;
 
 public class Leetcode2Test {
 
+    @Test
+    public void test1() throws Exception {
+        ListNode listNode1 = new Leetcode2().addTwoNumbers(toList(342), toList(465));
+        assertEquals("7 -> 0 -> 8", listNode1.toString());
+    }
+
+    private ListNode toList(int i) {
+
+        char[] charArray = String.valueOf(i).toCharArray();
+        ListNode next = null;
+        for (char c : charArray) {
+            ListNode listNode = new ListNode(Integer.parseInt(Character.toString(c)));
+            listNode.next = next;
+            next = listNode;
+        }
+        return next;
+    }
 }
